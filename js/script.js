2bb965b794340529576379f345893713e45e4621
wadii05 <didahwadii05@gmail.com>
2020-04-20 19:38:18 +0200

    audio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 function ajouter(){
     const url = document.getElementById("url").value;
     console.log(url)
-    document.getElementById("liste_music").innerHTML+='<li><a href="#">' + url+'</a> <button>supprimer</button></li>';
+    document.getElementById("liste_music").innerHTML+='<li onclick="toPlay()"><a href="#">' + url+'</a> <button>supprimer</button></li>';
+}
+
+
+
+function toPlay(){
+    document.getElementById("player").innerHTML=  '<source  src="http://michael.peopleofhonoronly.com/mp3/Total_Eclipse_of_the_Heart.mp3" type="audio/mpeg" ></source>'
 }
